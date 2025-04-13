4a2fc448795ea8e88d447ce4fe806b3bd4094523
Fadhlan Harashta <works.fadhlan@gmail.com>
2025-04-13 15:30:54 +0800

    Add fadhlan midterm

---
+++ midterm/112021222 - Muhammad Fadhlan Ashila Harashta/Midterm/github_scraper/github_scraper/items.py
@@ -0,0 +1,15 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class GithubScraperItem(scrapy.Item):
+    url = scrapy.Field()
+    about = scrapy.Field()
+    last_updated = scrapy.Field()
+    languages = scrapy.Field()
+    commits = scrapy.Field()
+    pass
